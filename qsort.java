9c577cedfe233af8c81713c5b36d27e873e1464a
Alexander Skobelev <al.skobelev@gmail.com>
2020-09-02 14:11:00 +0300

    WiP: Adapt to Habr

--- qsort.java
+++ qsort.java
@@ -15,19 +15,18 @@ private int partition(Object[] array, int begin, int end, Comparator cmp) {
 
 private int partition (Object[] array, int begin, int end, Comparator cmp)
 {
-    int    index = begin + RND.nextInt(end - begin + 1);
-    Object pivot = array[index];
+    int index = begin + RND.nextInt (end - begin + 1);
+    Object pivot = array [index];
 
-    swap(array, index, end);
+    swap (array, index, end);
 
     for (int i = index = begin; i < end; ++ i)
     {
-        if (cmp.compare(array[i], pivot) <= 0) {
-            swap(array, index++, i);
+        if (cmp.compare (array[i], pivot) <= 0) {
+            swap (array, index++, i);
         }
     }
-    
-    swap(array, index, end);
+    swap (array, index, end);
 
     return (index);
 }
